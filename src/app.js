b85dade0387fd00c6bb5046f8cde392545c1f38d
sixLUKEY <marcoslukerojo@gmail.com>
2023-06-07 16:40:45 +0200

    second commit

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ const cellphone = new CreateProduct( "Samsung Galaxy S22", "2022 model", "Cellph
 const cellphone2 = new CreateProduct( "Samsung Galaxy S23", "2022 model", "Cellphones", 20999, 25, "https:" )
 const laptop = new CreateProduct("Dell Asos", "25' screen", "Computer", 12999, 15, "htttps")
 const earphones = new CreateProduct("Galaxy Buds", "All black", "Accesories", 1999, 20, "https")
-const daisies = new CreateProduct("Daisy v5")
+const daisies = new CreateProduct("Daisy v5", "dandelion", "Accesories", 500, 20,  "https")
 console.log(products)
 
 const filteredArray = products.filter((product) => product.category === "Cellphones")
@@ -29,3 +29,6 @@ console.log(filteredArray)
 
 const sortedArray = filteredArray.filter((product) => product.price >= 22000)
 console.log(sortedArray)
+
+
+
